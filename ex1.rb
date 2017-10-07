a683d5e58d8716faeedc6072cffbe31c02ff9919
Josh <joshcane@yahoo.com>
2017-10-06 21:14:10 -0400

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,12 @@
+ text1 = " Bottles of beer on the wall "
+text2 = " Bottles of beer "
+text3 = " If one of those bottles should happen to fall "
+text4 = " No bottles of beer on the wall "
+text5 = " Go to the store and buy some more "
+
+i = 100
+until i <1
+puts "#{i}" + text1 + "#{i}"+ text2 +text3
+i -= 1
+puts text5
+end
